9a3d3ab2a94b4b414801989fc3bcb6b578fc36c8
Kaique Tassote <kaique_stassote@hotmail.com>
2021-09-29 19:29:34 -0300

    Projeto Aluraflix - Em Andamento

---
+++ flix_index.js
@@ -0,0 +1,9 @@
+var listaFilmes = ["Yesterday","A chegada","Escola de Rock"]
+
+listaFilmes.push("harry potter");
+
+document.write("<p>" + listaFilmes[0] + "</p>")
+document.write("<p>" + listaFilmes[1] + "</p>")
+document.write("<p>" + listaFilmes[2] + "</p>")
+document.write("<p>" + listaFilmes[3] + "</p>")
+
